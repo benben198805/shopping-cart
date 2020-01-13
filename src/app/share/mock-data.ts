1e883f3ba597a900b0fa1ce9ec3d275b0edcacc4
qlyan <benben198805@126.com>
2020-01-13 21:39:17 +0800

    add mock data

---
+++ src/app/share/mock-data.ts
@@ -0,0 +1,41 @@
+export const mockProducts = [{
+  name: "猕猴桃",
+  img: "assets/img/1.jpg",
+  price: 5,
+  count: 0
+}, {
+  name: "椰子",
+  img: "assets/img/2.jpg",
+  price: 20,
+  count: 0
+},
+  {
+    name: "梨",
+    img: "assets/img/3.jpg",
+    price: 1,
+    count: 0
+  },
+  {
+    name: "橙子",
+    img: "assets/img/4.jpg",
+    price: 6,
+    count: 0
+  },
+  {
+    name: "火龙果",
+    img: "assets/img/5.jpg",
+    price: 9,
+    count: 0
+  },
+  {
+    name: "百香果",
+    img: "assets/img/6.jpg",
+    price: 7,
+    count: 0
+  },
+  {
+    name: "香蕉",
+    img: "assets/img/7.jpg",
+    price: 2,
+    count: 0
+  }];
