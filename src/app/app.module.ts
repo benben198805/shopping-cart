bc648c2cdd972e914f3143335ec6560c6c866948
qlyan <benben198805@126.com>
2020-01-14 23:09:59 +0800

    move count-operator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { ProductItemComponent } from './product-list/product-item/product-item.component';
-import { CountOperatorComponent } from './share/component/count-operator/count-operator.component';
+import { CountOperatorComponent } from './shopping-cart/count-operator/count-operator.component';
 
 @NgModule({
   declarations: [
